b79cbda56cbf4388edaa9d70db7480624351c91a
dnaba-28 <dnathnaba@gmail.com>
2026-09-04 10:42:28 +0530

    Create DataWarehouse database and schemas
    This script creates a new database named DataWarehouse and sets up three schemas: bronze, silver, and gold.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,22 @@
+/* create database and schemas
 
+this script craete a new database named DataWarehouse after checking if it is already exists. if it exist drop it and recreate it . this 
+script set up 3 schemas bronze, silver,gold
+
+*/
+
+
+--CREATE DATABASE 'DATAWAREHOUSE'   
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+create schema bronze;
+GO
+create schema silver;
+GO
+create schema gold;
+GO
